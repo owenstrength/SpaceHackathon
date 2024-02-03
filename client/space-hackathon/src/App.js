d047c32da3efa1e7a331f6bbfa677067ab623c08
Owen Strength <Owenstrength@gmail.com>
2024-02-03 16:03:21 -0600

    added spinning earth and header

--- client/space-hackathon/src/App.js
+++ client/space-hackathon/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import React from 'react';
+import { Canvas, } from '@react-three/fiber';
+import HomePlanet from './HomePlanet';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{ height: '100%' }}>
+        <Header />
         {/* main page, has a circle div with 2 button options. sign in with spotify and use url. */}
-        <div className="circle">
+        <Canvas>
+          <ambientLight intensity={Math.PI / 2} />
+          <pointLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI * 1.5} />
+          <pointLight position={[-10, -10, -10]} decay={0.2} intensity={Math.PI / 4} />
+          <HomePlanet />
+        </Canvas>
+
+        {/*}
           <button className="button login">Sign in with Spotify</button>
           <button className="button playlist-url">Use Playlist URL</button>
-        </div>
+  */}
       </header>
     </div>
   );
---
+++ client/space-hackathon/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='main-header'>
+            <a className='header-text' href='https://github.com/owenstrength/SpaceHackathon'>Github</a>
+            <h1 className='header-text'>Music Planet</h1>
+            <a className='header-text' href='signin'>Sign-In</a>
+        </header>
+    );
+};
+
+export default Header;
