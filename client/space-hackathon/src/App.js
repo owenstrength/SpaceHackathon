9001421e32a23ada7d29f701835ee6289c61f4fa
Owen Strength <Owenstrength@gmail.com>
2024-02-03 14:17:14 -0600

    add three js and started on homepage

--- client/space-hackathon/src/App.js
+++ client/space-hackathon/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* main page, has a circle div with 2 button options. sign in with spotify and use url. */}
+        <div className="circle">
+          <button className="button login">Sign in with Spotify</button>
+          <button className="button playlist-url">Use Playlist URL</button>
+        </div>
       </header>
     </div>
   );
